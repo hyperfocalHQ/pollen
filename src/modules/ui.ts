0c88bf54c690285c2a17f372f7e4b55accd5488f
Madeleine Ostoja <madi@madeleineostoja.com>
2021-11-28 13:45:23 +1300

    Tweak elevations

--- src/modules/ui.ts
+++ src/modules/ui.ts
@@ -50,10 +50,11 @@ export default {
   elevation: {
     1: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
     2: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-    3: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-    4: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-    5: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-    6: '0 25px 50px -12px rgba(0, 0, 0, 0.25)'
+    3: '0 4px 6px -2px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.06)',
+    4: '0 12px 16px -4px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    5: '0 20px 24px -4px rgba(0, 0, 0, 0.1), 0 8px 8px -4px rgba(0, 0, 0, 0.04)',
+    6: '0 24px 48px -12px rgba(0, 0, 0, 0.25)',
+    7: '0 32px 64px -12px rgba(0, 0, 0, 0.2)'
   },
 
   /**
