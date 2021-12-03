18573de74bb184acefa1f5cea76a26c1603b4e6d
Madeleine Ostoja <madi@madeleineostoja.com>
2021-12-03 22:56:43 +1300

    remove number splitting for kebab cased variables

--- src/lib/formatModule.ts
+++ src/lib/formatModule.ts
@@ -1,18 +1,12 @@
-import Case from 'case';
+import { kebab } from 'case';
 import mapObject from 'map-obj';
 import type { PollenModule } from '..';
-
-Case.type('variable', function (s) {
-  return Case.kebab(s.match(/[a-zA-Z]+|[0-9]+/g)!.join('-'));
-});
 
 export function formatModule(module: PollenModule) {
   return Object.keys(module)
     .map((family) => {
       return mapObject(module[family as keyof typeof module], (key, value) => [
-        `--${(Case as any).variable(family)}-${(Case as any).variable(
-          String(key)
-        )}`,
+        `--${kebab(family)}-${kebab(String(key))}`,
         value
       ]);
     })
--- src/modules/colors.ts
+++ src/modules/colors.ts
@@ -5,71 +5,71 @@
 export default {
   color: {
     /* Greyscale */
-    grey50: '#f9fafb',
-    grey100: '#f2f4f5',
-    grey200: '#e8eaed',
-    grey300: '#d4d7dd',
-    grey400: '#a5aab4',
-    grey500: '#767c89',
-    grey600: '#555D6E',
-    grey700: '#3f4754',
-    grey800: '#2c343f',
-    grey900: '#10181C',
+    'grey-50': '#f9fafb',
+    'grey-100': '#f2f4f5',
+    'grey-200': '#e8eaed',
+    'grey-300': '#d4d7dd',
+    'grey-400': '#a5aab4',
+    'grey-500': '#767c89',
+    'grey-600': '#555D6E',
+    'grey-700': '#3f4754',
+    'grey-800': '#2c343f',
+    'grey-900': '#10181C',
     black: '#14141B',
     grey: 'var(--color-grey-500)',
 
     /* Red */
-    red300: '#fc8181',
-    red500: '#e53e3e',
-    red700: '#c53030',
+    'red-300': '#fc8181',
+    'red-500': '#e53e3e',
+    'red-700': '#c53030',
     red: 'var(--color-red-500)',
 
     /* Green */
-    green300: '#9ae6b4',
-    green500: '#48bb78',
-    green700: '#2f855a',
+    'green-300': '#9ae6b4',
+    'green-500': '#48bb78',
+    'green-700': '#2f855a',
     green: 'var(--color-green-500)',
 
     /* Blue */
-    blue300: '#63b3ed',
-    blue500: '#4299e1',
-    blue700: '#3182ce',
+    'blue-300': '#63b3ed',
+    'blue-500': '#4299e1',
+    'blue-700': '#3182ce',
     blue: 'var(--color-blue-500)',
 
     /* Pink */
-    pink300: '#fbb6ce',
-    pink500: '#ed64a6',
-    pink700: '#d53f8c',
+    'pink-300': '#fbb6ce',
+    'pink-500': '#ed64a6',
+    'pink-700': '#d53f8c',
     pink: 'var(--color-pink-500)',
 
     /* Purple */
-    purple300: '#b794f4',
-    purple500: '#805ad5',
-    purple700: '#6b46c1',
+    'purple-300': '#b794f4',
+    'purple-500': '#805ad5',
+    'purple-700': '#6b46c1',
     purple: 'var(--color-purple-500)',
 
     /* Teal */
-    teal300: '#81e6d9',
-    teal500: '#38b2ac',
-    teal700: '#2c7a7b',
+    'teal-300': '#81e6d9',
+    'teal-500': '#38b2ac',
+    'teal-700': '#2c7a7b',
     teal: 'var(--color-teal-500)',
 
     /* Yellow */
-    yellow300: '#faf089',
-    yellow500: '#ecc94b',
-    yellow700: '#d69e2e',
+    'yellow-300': '#faf089',
+    'yellow-500': '#ecc94b',
+    'yellow-700': '#d69e2e',
     yellow: 'var(--color-yellow-500)',
 
     /* Orange */
-    orange300: '#fbd38d',
-    orange500: '#ed8936',
-    orange700: '#dd6b20',
+    'orange-300': '#fbd38d',
+    'orange-500': '#ed8936',
+    'orange-700': '#dd6b20',
     orange: 'var(--color-orange-500)',
 
     /* Brown */
-    brown300: '#a1887f',
-    brown500: '#795548',
-    brown700: '#5d4037',
+    'brown-300': '#a1887f',
+    'brown-500': '#795548',
+    'brown-700': '#5d4037',
     brown: 'var(--color-brown-500)'
   }
 };
