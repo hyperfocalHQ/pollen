9c6761e6885f005cfe5070e623a0536715e0c448
Madeleine Ostoja <madi@madeleineostoja.com>
2022-09-12 21:09:04 +1200

    Alias elevation -> shadow in preperation for hard deprecate in v5. Fixes #64

--- src/modules/ui.ts
+++ src/modules/ui.ts
@@ -46,6 +46,7 @@ export default {
   /**
    * Elevation
    * Applied as box-shadow
+   * TODO: Deprecate in v5
    */
   elevation: {
     1: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
@@ -55,6 +56,18 @@ export default {
     5: '0 20px 24px -4px rgba(0, 0, 0, 0.1), 0 8px 8px -4px rgba(0, 0, 0, 0.04)',
     6: '0 24px 48px -12px rgba(0, 0, 0, 0.25)',
     7: '0 32px 64px -12px rgba(0, 0, 0, 0.2)'
+  },
+
+  /**
+   * Shadow
+   * Applied as box-shadow
+   */
+  shadow: {
+    xs: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+    sm: '0 4px 6px -2px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.06)',
+    md: '0 12px 16px -4px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    lg: '0 20px 24px -4px rgba(0, 0, 0, 0.1), 0 8px 8px -4px rgba(0, 0, 0, 0.04)',
+    xl: '0 24px 48px -12px rgba(0, 0, 0, 0.25)'
   },
 
   /**
